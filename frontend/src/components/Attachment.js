ac0b0dd0eba2e572a3534d94a970c54eb77ccb8d
Lucas Vicente <yugi.man22@gmail.com>
2019-07-23 11:25:07 +0100

    Fixed a missing dependency

--- frontend/src/components/Attachment.js
+++ frontend/src/components/Attachment.js
@@ -1,33 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
-const Attachment = (props) => {
+const Attachment = props => {
     const [attachment, setAttachment] = useState("");
     const [icon, setIcon] = useState(null);
 
-    
     useEffect(() => {
         let attachment_name = props.attachment.toLowerCase();
         let attachment_underscore = attachment_name.replace(/ /g, "_");
         setAttachment(attachment_name);
         setIcon(require(`../images/attachment/${attachment_underscore}.png`));
-    }, [])
+    }, [props.attachment]);
 
-    return (
-        icon ? 
-            <AttachmentWrapper color={props.color}>
-                <Icon>
-                    <img src={icon} alt="Holographic Sight" style={{ zoom: "5%" }}></img>
-                </Icon>
-                <Name>
-                    <p>{attachment}</p>
-                </Name>
-            </AttachmentWrapper> :
-        <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
-    )
-}
+    return icon ? (
+        <AttachmentWrapper color={props.color}>
+            <Icon>
+                <img
+                    src={icon}
+                    alt="Holographic Sight"
+                    style={{ zoom: "5%" }}
+                />
+            </Icon>
+            <Name>
+                <p>{attachment}</p>
+            </Name>
+        </AttachmentWrapper>
+    ) : (
+        <div className="lds-ring">
+            <div />
+            <div />
+            <div />
+            <div />
+        </div>
+    );
+};
 
-export default Attachment
+export default Attachment;
 
 const AttachmentWrapper = styled.div`
     margin-top: 10px;
@@ -35,11 +43,10 @@ const AttachmentWrapper = styled.div`
     grid-gap: 10px;
     grid-template-columns: repeat(2, 30px) auto;
     grid-template-rows: auto;
-    grid-template-areas:
-        ". attachment-icon attachment-name";
+    grid-template-areas: ". attachment-icon attachment-name";
     border: 1px solid var(--mainBlack);
     background-color: rgba(255, 255, 255, 0.2);
-`
+`;
 
 const Icon = styled.div`
     grid-area: attachment-icon;
@@ -48,7 +55,7 @@ const Icon = styled.div`
     img {
         margin-top: 50%;
     }
-`
+`;
 
 const Name = styled.div`
     grid-area: attachment-name;
@@ -58,4 +65,4 @@ const Name = styled.div`
         font-weight: 500;
         font-size: 18px;
     }
-`
+`;
--- frontend/src/components/OperatorList.js
+++ frontend/src/components/OperatorList.js
@@ -1,13 +1,13 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import styled from 'styled-components';
+import styled from "styled-components";
 import { AppContext } from "../context";
 import AnimatedImage from "./AnimatedImage";
- 
+
 const OperatorList = () => {
     const value = useContext(AppContext);
     const type = value.state.type;
-   /*  const operators = value.state.operatorsData.filter(operator => operator.type === type); */
+    /*  const operators = value.state.operatorsData.filter(operator => operator.type === type); */
 
     return (
         <OperatorListWrapper type={type}>
@@ -16,32 +16,39 @@ const OperatorList = () => {
                     const operator_name = operator.operator.toLowerCase();
                     const icon = require(`../images/operator/${operator_name}.svg`);
                     return (
-                        <Link style={{ textDecoration: 'none' }} to={{
-                            pathname: `/class/${operator_name}`,
-                            state: {
-                                index: index
-                            }
-                        }}>
-                            <AnimatedImage image={icon} name={operator_name}></AnimatedImage>
+                        <Link
+                            style={{ textDecoration: "none" }}
+                            to={{
+                                pathname: `/class/${operator_name}`,
+                                state: {
+                                    index: index
+                                }
+                            }}
+                        >
+                            <AnimatedImage image={icon} name={operator_name} />
                         </Link>
-                    )
+                    );
                 }
+                return null;
             })}
         </OperatorListWrapper>
-    )
-}
+    );
+};
 
-export default OperatorList
+export default OperatorList;
 
 const OperatorListWrapper = styled.div`
     display: grid;
     grid-gap: 10px;
     grid-template-columns: repeat(auto-fit, minmax(90px, 1fr));
     grid-template-rows: auto;
-    background: ${props => props.type === "attacker" ? "linear-gradient(214.1deg, #f58a07 0%, #df5104 100%)" : "linear-gradient(214.1deg, #0093e6 0%, #094f9b 100%)"} ;
+    background: ${props =>
+        props.type === "attacker"
+            ? "linear-gradient(214.1deg, #f58a07 0%, #df5104 100%)"
+            : "linear-gradient(214.1deg, #0093e6 0%, #094f9b 100%)"};
     border-radius: 10px;
 
     img {
         filter: drop-shadow(0px 0px 10px var(--mainBlack));
     }
-`
+`;
